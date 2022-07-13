54b1fb436f2f98c19968d848b564d126ae1f2a4f
Youngmin <jinmin645@gmail.com>
2022-07-13 12:51:05 +0000

    toy Code

--- Project/SOKC/gameController.cpp
+++ Project/SOKC/gameController.cpp
@@ -1,34 +1,58 @@
 #include <iostream>
-#include "Objects/gameObject.cpp"
 #include "jsonparser/jsonparser.cpp"
 #include <vector>
 #include <algorithm>
 
 class GameController
 {
-private:
-std::vector<Room> roomList;
 public:
-    void makeRoom(){
-        roomList.push_back(Room());
-    }
-    int checkRoom(int id){
-        int temp=-1;
-        std::for_each(roomList.begin(), roomList.end(), [&](Room room){
-            if(room.getId()==id){
-                if(room.playerCount()<16){
-                    temp=1;
-                }
+    void example(std::string in){
+        Json::Value data=toJson(in);
+        Json::Value toOne;
+        Json::Value toAll;
+        switch(data["Header"].asInt()){
+            case 0:
+            {
+                toOne["Header"]=0;
+                toOne["serverConnect"]=1;
+                outOne(toOne);
+                break;
             }
-        });
-        return temp;
-    }
-    Room& findRoom(int roomId){
-        for(int i=0; i<roomList.size();i++){
-            if(roomList[i].getId()==roomId){
-                return roomList[i];
+            case 1:
+            {
+                toOne["Header"]=1;
+                toAll["Header"]=1;
+                toOne["id"]=10039;
+                toOne["playerList"]="[(10032,\"YM\",3),(10053,\"SH\",4)]";
+                toAll["id"]=10039;
+                toAll["name"]=data["name"].asString();
+                outOne(toOne);
+                outAll(toAll);
+                break;
+            }
+            case 2:
+            {
+                toAll["Header"]=2;
+                toAll["id"]=data["id"];
+                toAll["colorId"]=data["colorId"].asInt();
+                outAll(toAll);
+                break;
+            }
+            case 5:
+            {
+                toAll["Header"]=5;
+                toAll["id"]=data["id"];
+                toAll["name"]="YM!";
+                outAll(toAll);
+                break;
             }
         }
     }
-    
+
+    void outOne(Json::Value out){
+        std::cout<<"To One : "<<out.toStyledString()<<std::endl;
+    }
+    void outAll(Json::Value out){
+        std::cout<<"To All : "<<out.toStyledString()<<std::endl;
+    }
 };

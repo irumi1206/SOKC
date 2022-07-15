ad5875b021c0acc7ca36bb8355fddeb55fb67416
Youngmin <jinmin645@gmail.com>
2022-07-15 07:40:30 +0000

    testfile with googletest

--- Project/SOKC/testController.cpp
+++ Project/SOKC/testController.cpp
@@ -1,40 +1,74 @@
 #include <iostream>
 #include "gameController.cpp"
+#include <gtest/gtest.h>
 
-int main(void){
+using namespace std;
 
-    // Game game;
-    // std::string json_example1 = "{\"Header\": 1,\"Player\": {\"id\": 4533 ,\"Name\":\"광오\"}}";
-    // std::string json_example2 = "{\"Header\": 1,\"Player\": {\"id\": 4856 ,\"Name\":\"Youngmin\"}}";
-    // std::string json_example3 = "{\"Header\": 2,\"Score\": {\"id\": 4856 ,\"data\": \"Kill\", \"value\":1}}";
-    // std::string json_example4 = "{\"Header\": 3,\"Score\": {\"id\": 4856 ,\"data\": \"Mission\", \"value\":1},\"Player\": {\"id\": 9561 ,\"Name\":\"Sung Ha\"}}";
-    // std::string json_example5 = "{\"Header\": 2,\"Score\": {\"id\": 4856 ,\"data\": \"Vote\", \"value\":1}}";
-    // std::string json_example6 = "{\"Header\": 2,\"Score\": {\"id\": 4856 ,\"data\": \"Kill\", \"value\":1}}";
-    // std::string json_example7 = "{\"Header\": 2,\"Score\": {\"id\": 4856 ,\"data\": \"Vote\", \"value\":0}}";
-    // std::string json_example8 = "{\"Header\": 2,\"Score\": {\"id\": 4856 ,\"data\": \"Vote\", \"value\":0}}";
-    // std::string json_example9 = "{\"Header\": 2,\"Score\": {\"id\": 4856 ,\"data\": \"Vote\", \"value\":0}}";
-    // std::string json_example10 = "{\"Header\": 4,\"Kill\": {\"id\": 4856 ,\"victim\": 4533}}";
-    // game.control(json_example1);
-    // game.control(json_example2);
-    // //game.flagcontrol(toJson(json_example3));
-    // game.control(json_example4);
-    // game.control(json_example5);
-    // //game.flagcontrol(toJson(json_example6));
-    // game.control(json_example7);
-    // game.control(json_example8);
-    // game.control(json_example9);
-    // game.control(json_example10);
+TEST(HEADER_0,room_check){
+    Controller controller=Controller();
+    Json::Value out;
+    Json::Value toOne;
+    toOne["Header"]=0;
+    toOne["serverConnect"]=1;
+    out["toOne"]=toOne;
+    EXPECT_EQ(controller.control("{\"Header\":0,\"roomId\":100}"),out);
+}
 
-    
+TEST(HEADER_1,room_check){
+    Controller controller=Controller();
+    Json::Value out;
+    Json::Value toOne;
+    Json::Value toAll;
+    toOne["Header"]=1;
+    toOne["id"]=10003;
+    toOne["playerList"]=true;
+    toAll["Header"]=1;
+    toAll["id"]=10003;
+    toAll["name"]="YM";
+    out["toOne"]=toOne;
+    out["toAll"]=toAll;
+    EXPECT_EQ(controller.control("{\"Header\":1,\"roomId\":100,\"name\":\"YM\"}"),out);
+    for(int i=0;i<15;i++){
+        controller.control("{\"Header\":1,\"roomId\":100,\"name\":\"YM\"}");
+        EXPECT_EQ(controller.game.countPlayers(),i+2);
+    }
+    Json::Value out1;
+    Json::Value toOne1;
+    toOne1["Header"]=0;
+    toOne1["serverConnect"]=-1;
+    out1["toOne"]=toOne1;
+    EXPECT_EQ(controller.control("{\"Header\":0,\"roomId\":100}"),out1);
+}
+
+TEST(HEADER_2,color_change_check){
+    Controller controller=Controller();
+    int id=controller.game.joinPlayer("YM");
+    Json::Value out;
+    Json::Value toAll;
+    toAll["Header"]=2;
+    toAll["id"]=id;
+    toAll["colorId"]=4;
+    out["toAll"]=toAll;
+    string temp="{\"Header\":2,\"id\":"+to_string(id)+",\"colorId\":4}";
+    EXPECT_EQ(controller.control(temp),out);
+}
+
+TEST(HEADER_5,exit_check){
+    Controller controller=Controller();
+    int id=controller.game.joinPlayer("YM");
+    Json::Value out;
+    Json::Value toAll;
+    toAll["Header"]=5;
+    toAll["id"]=id;
+    out["toAll"]=toAll;
+    string temp="{\"Header\":5,\"id\":"+to_string(id)+"}";
+    EXPECT_EQ(controller.control(temp),out);
+}
 
 
 
-    // return 0;
 
-    GameController game=GameController();
-    game.example("{\"Header\": 0,\"roomId\": 100}");
-    game.example("{\"Header\": 1,\"roomId\":100,\"name\": \"youngman\"}");
-    game.example("{\"Header\": 2,\"id\":10039,\"colorId\": 4}");
-    game.example("{\"Header\": 5,\"id\":10039}");
-    return 0;
+int main(int argc, char **argv){
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }

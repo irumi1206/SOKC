1010567c48dceb1d8d1231e3a92c9b0f2ba06dbd
Youngmin <jinmin645@gmail.com>
2022-07-17 06:12:33 +0000

    Header 30~32

--- Project/SOKC/gameController.cpp
+++ Project/SOKC/gameController.cpp
@@ -11,7 +11,7 @@ public:
     Controller(){
         this->game=Game();
     }
-    Json::Value control(std::string in){
+    auto control(std::string in){
         Json::Value data=toJson(in);
         Json::Value out;
         switch(data["Header"].asInt()){
@@ -66,10 +66,64 @@ public:
                 out["toAll"]=toAll;
                 return out;
             }
+            case 30:
+            {
+                game.findPlayer(data["id"].asInt()).setPosition(data["x"].asFloat(),data["y"].asFloat());
+                out["Header"]=1;
+                return out;
+            }
+            case 31:
+            {
+                int id=data["id"].asInt();
+                int victim=data["victim"].asInt();
+                game.findPlayer(victim).dead();
+                game.findPlayer(id).addKillScore(1);
+                Json::Value toOne;
+                Json::Value toAll;
+                toOne["Header"]=31;
+                toAll["Header"]=31;
+                toOne["id"]=id;
+                toAll["id"]=victim;
+                out["toOne"]=toOne;
+                out["toAll"]=toAll;
+                return out;
+            }
+            case 32:
+            {
+                int id=data["id"].asInt();
+                int missionId=data["missionId"].asInt();
+                game.findPlayer(id).finishMission(missionId);
+                game.findPlayer(id).addMissionScore(1);
+                return out;
+            }
             default:
             {
                 return out;
             }
         }
+    }
+    Json::Value positions(){
+        Json::Value out;
+        Json::Value toAll;
+        toAll["Header"]=30;
+        int i=0;
+        std::string positions="[";
+        std::for_each(game.playerList.begin(), game.playerList.end(), [&](Player& player){
+            positions+="(";
+            positions+="\"id\":"+std::to_string(player.getId());
+            std::tuple<float,float> position=player.getPosition();
+            positions+=",\"x\":"+std::to_string(std::get<0>(position));
+            positions+=",\"y\":"+std::to_string(std::get<1>(position));
+            if(i==game.countPlayers()-1){
+                positions+=")";
+            }else{
+                positions+="),";
+            }
+            i++;
+        });
+        positions+="]";
+        toAll["playerPositions"]=positions;
+        out["toAll"]=toAll;
+        return out;
     }
 };

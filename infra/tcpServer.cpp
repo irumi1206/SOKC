7c08cc63351b4acbbb99b747bba89931edfb8602
Youngmin <jinmin645@gmail.com>
2022-07-20 10:09:16 +0000

    restore of main function due to disconnection of ec2

--- infra/tcpServer.cpp
+++ infra/tcpServer.cpp
@@ -12,29 +12,36 @@
 #define LEN 1024
 int main(int argc, char const* argv[])
 {
-    int server_fd, new_socket, valread;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-    const char* hello = "server received the message";
+    tcpService service=tcpService();
+    service.setPort(PORT);
+    service.start();
+
+
+    // int server_fd, new_socket, valread;
+    // struct sockaddr_in address;
+    // int opt = 1;
+    // int addrlen = sizeof(address);
+    // const char* hello = "server received the message";
   
 
-    tcpConnectionSocket masterSocket=tcpConnectionSocket();
-    masterSocket.setPort(PORT);
-    masterSocket.getReady();
-    tcpConnection temp=masterSocket.getConnectedSocket();
+    // tcpConnectionSocket masterSocket=tcpConnectionSocket();
+    // masterSocket.setPort(PORT);
+    // masterSocket.getReady();
+    // tcpConnection temp=masterSocket.getConnectedSocket();
 
-    while(1){
-        char inp[LEN]={0};
-        temp.in(inp,LEN);
-        printf("%s\n",inp);
-        char out[LEN]="hello";
-        temp.out(out);
-    }
+    // while(1){
+    //     char inp[LEN]={0};
+    //     temp.in(inp,LEN);
+    //     printf("%s\n",inp);
+    //     char out[LEN]="hello";
+    //     temp.out(out);
+    // }
+
+
     
   // closing the connected socket
-    close(new_socket);
+    //close(new_socket);
   // closing the listening socket
-    shutdown(server_fd, SHUT_RDWR);
+    //shutdown(server_fd, SHUT_RDWR);
     return 0;
 }

a3c05135b3a7bc273f835d7d46a9a3c908a6574c
Youngmin <jinmin645@gmail.com>
2022-07-29 03:16:22 +0000

    fixed test code with new headers

--- test/testController.cpp
+++ test/testController.cpp
@@ -14,7 +14,7 @@ TEST(HEADER_0,room_check){
     out["toOne"]=toOne;
     EXPECT_EQ(controller.control("{\"Header\":0,\"Content\":{\"roomId\":100}}"),out);
 }
-
+//룸 입장, 다른 사람들에게 입장한 플레이어 정보(2), 입장한 플레이어 색(3)
 TEST(HEADER_1,room_check){
     Controller controller=Controller();
     Json::Value expect1=controller.control("{\"Header\":1,\"Content\":{\"roomId\":100,\"name\":\"YM\"}}");
@@ -32,7 +32,7 @@ TEST(HEADER_1,room_check){
     other2["Header"]=3;
     other2["Content"];
     other2["Content"]["id"]=expect1["toOne"]["Content"]["id"];
-    other2["Content"]["colorId"]=0;
+    other2["Content"]["colorId"]=1;
     out["toOne"]=toOne;
     out["other"]=other;
     out["other2"]=other2;
@@ -79,50 +79,69 @@ TEST(HEADER_1,room_check){
 //     EXPECT_EQ(controller.control("{\"Header\":1,\"Content\":{\"roomId\":100,\"name\":\"YMK\"}}"),out);
 // }
 
-// TEST(HEADER_2,color_change_check){
-//     Controller controller=Controller();
-//     int id=controller.game.joinPlayer("YM");
-//     Json::Value out;
-//     Json::Value toAll;
-//     toAll["Header"]=2;
-//     toAll["Content"];
-//     toAll["Content"]["id"]=id;
-//     toAll["Content"]["colorId"]=4;
-//     out["toAll"]=toAll;
-//     string temp="{\"Header\":2,\"Content\":{\"id\":"+to_string(id)+",\"colorId\":4}}";
-//     EXPECT_EQ(controller.control(temp),out);
-// }
+TEST(HEADER_3,color_change_check){
+    Controller controller=Controller();
+    int id=controller.game.joinPlayer("YM");
+    Json::Value out;
+    Json::Value other;
+    other["Header"]=3;
+    other["Content"];
+    other["Content"]["id"]=id;
+    other["Content"]["colorId"]=4;
+    out["other"]=other;
+    string temp="{\"Header\":3,\"Content\":{\"id\":"+to_string(id)+",\"colorId\":4}}";
+    EXPECT_EQ(controller.control(temp),out);
+}
 
-// TEST(HEADER_5,exit_check){
-//     Controller controller=Controller();
-//     int id=controller.game.joinPlayer("YM");
-//     Json::Value out;
-//     Json::Value toAll;
-//     toAll["Header"]=5;
-//     toAll["Content"];
-//     toAll["Content"]["id"]=id;
-//     out["toAll"]=toAll;
-//     string temp="{\"Header\":5,\"Content\":{\"id\":"+to_string(id)+"}}";
-//     EXPECT_EQ(controller.game.countPlayers(),1);
-//     EXPECT_EQ(controller.control(temp),out);
-//     EXPECT_EQ(controller.game.countPlayers(),0);
-// }
+TEST(HEADER_4,room_list_check){
+    Controller controller=Controller();
+    int id=controller.game.joinPlayer("YM");
+    controller.game.findPlayer(id).setColor(3);
+    Json::Value out;
+    Json::Value toOne;
+    toOne["Header"]=4;
+    toOne["Content"];
+    toOne["Content"]["playerList"]="[{\"id\":"+to_string(id)+",\"name\":\"YM\",\"color\":3}]";
+    out["toOne"]=toOne;
+    EXPECT_EQ(controller.control("{\"Header\":4}"),out);
+    int id1=controller.game.joinPlayer("KSH");
+    controller.game.findPlayer(id1).setColor(5);
+    toOne["Content"]["playerList"]="[{\"id\":"+to_string(id)+",\"name\":\"YM\",\"color\":3},{\"id\":"+to_string(id1)+",\"name\":\"KSH\",\"color\":5}]";
+    out["toOne"]=toOne;
+    EXPECT_EQ(controller.control("{\"Header\":4}"),out);
+}
 
-// TEST(HEADER_30,moving_check){
-//     Controller controller=Controller();
-//     int id1=controller.game.joinPlayer("YM");
-//     int id2=controller.game.joinPlayer("SH");
-//     int id3=controller.game.joinPlayer("SJ");
-//     controller.control("{\"Header\":30,\"Content\":{\"id\":"+std::to_string(id1)+",\"x\":3.140000,\"y\":6.20000}}");
-//     controller.control("{\"Header\":30,\"Content\":{\"id\":"+std::to_string(id2)+",\"x\":3.530000,\"y\":0.700000}}");
-//     controller.control("{\"Header\":30,\"Content\":{\"id\":"+std::to_string(id3)+",\"x\":6.310000,\"y\":10.300000}}");
-//     tuple<float,float> temp1=make_tuple<float,float>(3.14,6.2);
-//     EXPECT_EQ(controller.game.findPlayer(id1).getPosition(), temp1);
-//     tuple<float,float> temp2=make_tuple<float,float>(3.53,0.7);
-//     EXPECT_EQ(controller.game.findPlayer(id2).getPosition(), temp2);
-//     tuple<float,float> temp3=make_tuple<float,float>(6.31,10.3);
-//     EXPECT_EQ(controller.game.findPlayer(id3).getPosition(), temp3);
-// }
+
+TEST(HEADER_5,exit_check){
+    Controller controller=Controller();
+    int id=controller.game.joinPlayer("YM");
+    Json::Value out;
+    Json::Value other;
+    other["Header"]=5;
+    other["Content"];
+    other["Content"]["id"]=id;
+    out["other"]=other;
+    string temp="{\"Header\":5,\"Content\":{\"id\":"+to_string(id)+"}}";
+    EXPECT_EQ(controller.game.countPlayers(),1);
+    EXPECT_EQ(controller.control(temp),out);
+    EXPECT_EQ(controller.game.countPlayers(),0);
+}
+
+TEST(HEADER_30,moving_check){
+    Controller controller=Controller();
+    int id1=controller.game.joinPlayer("YM");
+    int id2=controller.game.joinPlayer("SH");
+    int id3=controller.game.joinPlayer("SJ");
+    controller.control("{\"Header\":30,\"Content\":{\"id\":"+std::to_string(id1)+",\"x\":3.140000,\"y\":6.20000}}");
+    controller.control("{\"Header\":30,\"Content\":{\"id\":"+std::to_string(id2)+",\"x\":3.530000,\"y\":0.700000}}");
+    controller.control("{\"Header\":30,\"Content\":{\"id\":"+std::to_string(id3)+",\"x\":6.310000,\"y\":10.300000}}");
+    tuple<float,float> temp1=make_tuple<float,float>(3.14,6.2);
+    EXPECT_EQ(controller.game.findPlayer(id1).getPosition(), temp1);
+    tuple<float,float> temp2=make_tuple<float,float>(3.53,0.7);
+    EXPECT_EQ(controller.game.findPlayer(id2).getPosition(), temp2);
+    tuple<float,float> temp3=make_tuple<float,float>(6.31,10.3);
+    EXPECT_EQ(controller.game.findPlayer(id3).getPosition(), temp3);
+}
 
 // TEST(HEADER_30,get_position_check){
 //     Controller controller=Controller();
@@ -141,90 +160,96 @@ TEST(HEADER_1,room_check){
 //     EXPECT_EQ(controller.positions(),out);
 // }
 
-// TEST(HEADER_31,kill_check){
-//     Controller controller=Controller();
-//     int id1=controller.game.joinPlayer("YM");
-//     int id2=controller.game.joinPlayer("SH");
-//     int id3=controller.game.joinPlayer("SJ");
-//     EXPECT_EQ(controller.game.findPlayer(id1).isDie(),false);
-//     EXPECT_EQ(controller.game.findPlayer(id2).isDie(),false);
-//     EXPECT_EQ(controller.game.findPlayer(id3).isDie(),false);
-//     //different team(molgo kill poolc)
-//     Json::Value out;
-//     Json::Value toAll;
-//     Json::Value toOne;
-//     toAll["Header"]=31;
-//     toOne["Header"]=31;
-//     toAll["Content"];
-//     toOne["Content"];
-//     toAll["Content"]["id"]=id2;
-//     toOne["Content"]["id"]=id1;
-//     out["toOne"]=toOne;
-//     out["toAll"]=toAll;
-//     controller.game.findPlayer(id1).setRole(25);
-//     controller.game.findPlayer(id2).setRole(2);
-//     controller.game.findPlayer(id3).setRole(25);
-//     EXPECT_EQ(controller.control("{\"Header\":31,\"Content\":{\"id\":"+to_string(id1)+",\"victim\":"+to_string(id2)+"}}"),out);
-//     EXPECT_EQ(controller.game.findPlayer(id2).isDie(),true);
-//     EXPECT_EQ(controller.game.findPlayer(id1).getKillScore(),1);
-//     //different team(poolc kill molgo)
-//     toAll["Header"]=31;
-//     toOne["Header"]=31;
-//     toAll["Content"];
-//     toOne["Content"];
-//     toAll["Content"]["id"]=id3;
-//     toOne["Content"]["id"]=id2;
-//     out["toOne"]=toOne;
-//     out["toAll"]=toAll;
-//     controller.game.findPlayer(id2).live();
-//     EXPECT_EQ(controller.control("{\"Header\":31,\"Content\":{\"id\":"+to_string(id2)+",\"victim\":"+to_string(id3)+"}}"),out);
-//     EXPECT_EQ(controller.game.findPlayer(id3).isDie(),true);
-//     EXPECT_EQ(controller.game.findPlayer(id2).getKillScore(),1);
-//     //same team(molgo kill molgo)
-//     toAll["Header"]=31;
-//     toOne["Header"]=31;
-//     toAll["Content"];
-//     toOne["Content"];
-//     toAll["Content"]["id"]=id1;
-//     toOne["Content"]["id"]=id3;
-//     out["toOne"]=toOne;
-//     out["toAll"]=toAll;
-//     controller.game.findPlayer(id3).live();
-//     EXPECT_EQ(controller.control("{\"Header\":31,\"Content\":{\"id\":"+to_string(id3)+",\"victim\":"+to_string(id1)+"}}"),out);
-//     EXPECT_EQ(controller.game.findPlayer(id1).isDie(),true);
-//     EXPECT_EQ(controller.game.findPlayer(id3).getKillScore(),-1);
-// }
+TEST(HEADER_32,kill_check){
+    Controller controller=Controller();
+    int id1=controller.game.joinPlayer("YM");
+    int id2=controller.game.joinPlayer("SH");
+    int id3=controller.game.joinPlayer("SJ");
+    EXPECT_EQ(controller.game.findPlayer(id1).isDie(),false);
+    EXPECT_EQ(controller.game.findPlayer(id2).isDie(),false);
+    EXPECT_EQ(controller.game.findPlayer(id3).isDie(),false);
+    //different team(molgo kill poolc)
+    Json::Value out;
+    Json::Value other;
+    Json::Value toOne;
+    other["Header"]=32;
+    toOne["Header"]=32;
+    other["Content"];
+    toOne["Content"];
+    other["Content"]["id"]=0;
+    other["Content"]["victim"]=id2;
+    toOne["Content"]["id"]=id1;
+    toOne["Content"]["victim"]=id2;
+    out["toOne"]=toOne;
+    out["other"]=other;
+    controller.game.findPlayer(id1).setRole(25);
+    controller.game.findPlayer(id2).setRole(2);
+    controller.game.findPlayer(id3).setRole(25);
+    EXPECT_EQ(controller.control("{\"Header\":32,\"Content\":{\"id\":"+to_string(id1)+",\"victim\":"+to_string(id2)+"}}"),out);
+    EXPECT_EQ(controller.game.findPlayer(id2).isDie(),true);
+    EXPECT_EQ(controller.game.findPlayer(id1).getKillScore(),1);
+    //different team(poolc kill molgo)
+    other["Header"]=32;
+    toOne["Header"]=32;
+    other["Content"];
+    toOne["Content"];
+    other["Content"]["id"]=0;
+    other["Content"]["victim"]=id3;
+    toOne["Content"]["id"]=id2;
+    toOne["Content"]["victim"]=id3;
+    out["toOne"]=toOne;
+    out["other"]=other;
+    controller.game.findPlayer(id2).live();
+    EXPECT_EQ(controller.control("{\"Header\":32,\"Content\":{\"id\":"+to_string(id2)+",\"victim\":"+to_string(id3)+"}}"),out);
+    EXPECT_EQ(controller.game.findPlayer(id3).isDie(),true);
+    EXPECT_EQ(controller.game.findPlayer(id2).getKillScore(),1);
+    //same team(molgo kill molgo)
+    other["Header"]=32;
+    toOne["Header"]=32;
+    other["Content"];
+    toOne["Content"];
+    other["Content"]["id"]=0;
+    other["Content"]["victim"]=id1;
+    toOne["Content"]["id"]=id3;
+    toOne["Content"]["victim"]=id1;
+    out["toOne"]=toOne;
+    out["other"]=other;
+    controller.game.findPlayer(id3).live();
+    EXPECT_EQ(controller.control("{\"Header\":32,\"Content\":{\"id\":"+to_string(id3)+",\"victim\":"+to_string(id1)+"}}"),out);
+    EXPECT_EQ(controller.game.findPlayer(id1).isDie(),true);
+    EXPECT_EQ(controller.game.findPlayer(id3).getKillScore(),-1);
+}
 
-// TEST(HEADER_32,mission_check){
-//     Controller controller=Controller();
-//     int id1=controller.game.joinPlayer("YM");
-//     int id2=controller.game.joinPlayer("SH");
-//     int id3=controller.game.joinPlayer("SJ");
-//     controller.game.findPlayer(id1).addMission(1);
-//     controller.game.findPlayer(id1).addMission(2);
-//     controller.game.findPlayer(id1).addMission(3);
-//     controller.game.findPlayer(id2).addMission(4);
-//     controller.game.findPlayer(id2).addMission(5);
-//     controller.game.findPlayer(id2).addMission(6);
-//     EXPECT_EQ(controller.game.findPlayer(id1).countMission(),3);
-//     EXPECT_EQ(controller.game.findPlayer(id2).countMission(),3);
-//     EXPECT_EQ(controller.game.findPlayer(id3).countMission(),0);
-//     controller.control("{\"Header\":32,\"Content\":{\"id\":"+to_string(id1)+",\"missionId\":1}}");
-//     EXPECT_EQ(controller.game.findPlayer(id1).countMission(),2);
-//     EXPECT_EQ(controller.game.findPlayer(id1).getMissionScore(),1);
-//     controller.game.findPlayer(id3).assignMission(3);
-//     EXPECT_EQ(controller.game.findPlayer(id3).countMission(),3);
-//     vector<int> missions=controller.game.findPlayer(id3).getMission();
-//     controller.control("{\"Header\":32,\"Content\":{\"id\":"+to_string(id3)+",\"missionId\":"+to_string(missions[0])+"}}");
-//     EXPECT_EQ(controller.game.findPlayer(id3).countMission(),2);
-//     EXPECT_EQ(controller.game.findPlayer(id3).getMissionScore(),1);
-//     controller.control("{\"Header\":32,\"Content\":{\"id\":"+to_string(id3)+",\"missionId\":"+to_string(missions[1])+"}}");
-//     EXPECT_EQ(controller.game.findPlayer(id3).countMission(),1);
-//     EXPECT_EQ(controller.game.findPlayer(id3).getMissionScore(),2);
-//     controller.control("{\"Header\":32,\"Content\":{\"id\":"+to_string(id3)+",\"missionId\":"+to_string(missions[2])+"}}");
-//     EXPECT_EQ(controller.game.findPlayer(id3).countMission(),0);
-//     EXPECT_EQ(controller.game.findPlayer(id3).getMissionScore(),3);
-// }
+TEST(HEADER_33,mission_check){
+    Controller controller=Controller();
+    int id1=controller.game.joinPlayer("YM");
+    int id2=controller.game.joinPlayer("SH");
+    int id3=controller.game.joinPlayer("SJ");
+    controller.game.findPlayer(id1).addMission(1);
+    controller.game.findPlayer(id1).addMission(2);
+    controller.game.findPlayer(id1).addMission(3);
+    controller.game.findPlayer(id2).addMission(4);
+    controller.game.findPlayer(id2).addMission(5);
+    controller.game.findPlayer(id2).addMission(6);
+    EXPECT_EQ(controller.game.findPlayer(id1).countMission(),3);
+    EXPECT_EQ(controller.game.findPlayer(id2).countMission(),3);
+    EXPECT_EQ(controller.game.findPlayer(id3).countMission(),0);
+    controller.control("{\"Header\":33,\"Content\":{\"id\":"+to_string(id1)+",\"missionId\":1}}");
+    EXPECT_EQ(controller.game.findPlayer(id1).countMission(),2);
+    EXPECT_EQ(controller.game.findPlayer(id1).getMissionScore(),1);
+    controller.game.findPlayer(id3).assignMission(3);
+    EXPECT_EQ(controller.game.findPlayer(id3).countMission(),3);
+    vector<int> missions=controller.game.findPlayer(id3).getMission();
+    controller.control("{\"Header\":33,\"Content\":{\"id\":"+to_string(id3)+",\"missionId\":"+to_string(missions[0])+"}}");
+    EXPECT_EQ(controller.game.findPlayer(id3).countMission(),2);
+    EXPECT_EQ(controller.game.findPlayer(id3).getMissionScore(),1);
+    controller.control("{\"Header\":33,\"Content\":{\"id\":"+to_string(id3)+",\"missionId\":"+to_string(missions[1])+"}}");
+    EXPECT_EQ(controller.game.findPlayer(id3).countMission(),1);
+    EXPECT_EQ(controller.game.findPlayer(id3).getMissionScore(),2);
+    controller.control("{\"Header\":33,\"Content\":{\"id\":"+to_string(id3)+",\"missionId\":"+to_string(missions[2])+"}}");
+    EXPECT_EQ(controller.game.findPlayer(id3).countMission(),0);
+    EXPECT_EQ(controller.game.findPlayer(id3).getMissionScore(),3);
+}
 
 
 

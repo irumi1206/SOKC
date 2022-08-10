e8a6738bfb08a13d9af5792940bb89362cb45a22
Youngmin <jinmin645@gmail.com>
2022-08-10 05:54:54 +0000

    by removing poolclist, morgolist, midlist, fix testfile

--- test/Player/testGame.cpp
+++ test/Player/testGame.cpp
@@ -50,12 +50,12 @@ TEST(role_test,assign_Role){
     game.setMidCount(2);
     game.assignRole();
     vector<Player> list=game.getPlayers();
-    vector<int> poolcList=game.getPoolc();
-    vector<int> molgoList=game.getMolgo();
-    vector<int> midList=game.getMid();
-    EXPECT_EQ(poolcList.size(),4);
-    EXPECT_EQ(molgoList.size(),2);
-    EXPECT_EQ(midList.size(),2);
+    // vector<int> poolcList=game.getPoolc();
+    // vector<int> molgoList=game.getMolgo();
+    // vector<int> midList=game.getMid();
+    // EXPECT_EQ(poolcList.size(),4);
+    // EXPECT_EQ(molgoList.size(),2);
+    // EXPECT_EQ(midList.size(),2);
 }
 
 TEST(color_test,assign_color_auto){
@@ -99,12 +99,43 @@ TEST(start_game,game_start){
     int id7=game.joinPlayer("player7");
     int id8=game.joinPlayer("player8");
     game.gameStart();
-    EXPECT_EQ(game.getMolgo().size(),2);
-    EXPECT_EQ(game.getPoolc().size(),4);
-    EXPECT_EQ(game.getMid().size(),2);
+    // EXPECT_EQ(game.getMolgo().size(),2);
+    // EXPECT_EQ(game.getPoolc().size(),4);
+    // EXPECT_EQ(game.getMid().size(),2);
     for_each(game.playerList.begin(),game.playerList.end(),[&](Player& player){
         EXPECT_EQ(player.getMission().size(),3);
     });
+}
+
+TEST(Vote,voting_all){
+    Game game=Game();
+    int id1=game.joinPlayer("player1");
+    int id2=game.joinPlayer("player2");
+    int id3=game.joinPlayer("player3");
+    int id4=game.joinPlayer("player4");
+    int id5=game.joinPlayer("player5");
+    int id6=game.joinPlayer("player6");
+    int id7=game.joinPlayer("player7");
+    int id8=game.joinPlayer("player8");
+    game.putVote(id1,id2);
+    game.putVote(id2,id2);
+    game.putVote(id3,id2);
+    game.putVote(id4,id1);
+    game.putVote(id5,id1);
+    game.putVote(id6,id4);
+    game.putVote(id7,id5);
+    game.putVote(id8,id6);
+    EXPECT_EQ(game.calculateVoteDead(),id2);
+    game.clearVoteStorage();
+    game.putVote(id1,id2);
+    game.putVote(id2,id2);
+    game.putVote(id3,id2);
+    game.putVote(id4,id1);
+    game.putVote(id5,id1);
+    game.putVote(id6,id1);
+    game.putVote(id7,id5);
+    game.putVote(id8,id6);
+    EXPECT_EQ(game.calculateVoteDead(),-1);
 }
 
 
